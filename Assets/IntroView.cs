ed57f0e40b57a798da975a3c8de418b369d59879
hyeminahn <oraksa_gamestudio@gmail.com>
2026-01-07 13:03:26 +0900

    Add new social media button assets and their metadata
    - Added social_apple-btn.png and social_apple-btn.png.meta for Apple social button.
    - Added social_apple-trans.png and social_apple-trans.png.meta for Apple transparent social button.
    - Added social_google-btn.png and social_google-btn.png.meta for Google social button.
    - Added social_google-trans.png and social_google-trans.png.meta for Google transparent social button.
    
    These assets include various texture settings and configurations for optimal use in the application.

--- Assets/IntroView.cs
+++ Assets/IntroView.cs
@@ -1,16 +1,13 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class IntroView : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Image introBackground;
+    [SerializeField] private TextMeshProUGUI progressValueText;
+    [SerializeField] private TextMeshProUGUI progressMessageText;
+    [SerializeField] private Slider progressBarSlider;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
